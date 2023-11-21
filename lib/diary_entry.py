b1dbbfbdba81ba7e7a942366a9a4b7eec1963cc5
benjamin <b.james.neustadt@gmail.com>
2023-11-21 16:38:03 +0000

    Test and implement reading chunk
    * added some variables in the testing suite for testing purposes(i.e. 'one_hundred_words')

--- lib/diary_entry.py
+++ lib/diary_entry.py
@@ -1,13 +1,12 @@
 # File: lib/diary_entry.py
 
 class DiaryEntry:
-#    # Public Properties:
-#    #   title: a string
-#    #   contents: a string
 #
     def __init__(self, title, contents):
         self.title = title
         self.contents = contents
+        self.chunk = []
+        self.remaining_chunk_start = 0
 
     def count_words(self):
         list_of_contents_words = self.contents.split()
@@ -18,17 +17,10 @@ class DiaryEntry:
         number_of_words = len(list_of_words)
         return number_of_words / wpm
 
-#
-#    def reading_chunk(self, wpm, minutes):
-#        # Parameters:
-#        #   wpm: an integer representing the number of words the user can read
-#        #        per minute
-#        #   minutes: an integer representing the number of minutes the user has
-#        #            to read
-#        # Returns:
-#        #   A string representing a chunk of the contents that the user could
-#        #   read in the given number of minutes.
-#        # If called again, `reading_chunk` should return the next chunk,
-#        # skipping what has already been read, until the contents is fully read.
-#        # The next call after that it should restart from the beginning.
-#        pass
+    def reading_chunk(self, wpm, minutes):
+        max_words = wpm * minutes
+        list_of_words = (self.contents).split()
+        remaining_chunk = list_of_words[self.remaining_chunk_start:self.remaining_chunk_start + max_words]
+        self.remaining_chunk_start += len(remaining_chunk)
+        report_chunk = ' '.join(remaining_chunk)
+        return report_chunk
--- tests/test_diary_entry.py
+++ tests/test_diary_entry.py
@@ -1,5 +1,10 @@
 from lib.diary_entry import *
 
+one_hundred_words = "one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one"
+two_hundred_words = "one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one"
+one_hundred_words_2 = "two two two two two two two two two two two two two two two two two two two two two two two two two two two two two two two two two two two two two two two two two two two two two two two two two two two two two two two two two two two two two two two two two two two two two two two two two two two two two two two two two two two two two two two two two two two two two two two two two two two two"
+mixture_of_words = "one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one two two two two two two two two two two two two two two two two two two two two two two two two two two two two two two two two two two two two two two two two two two two two two two two two two two two two two two two two two two two two two two two two two two two two two two two two two two two two two two two two two two two two two two two two two two two two two two two two two two two two"
+
 def test_diary_entry_exists():
     diary_entry = DiaryEntry("Book title", "some stuff")
 
@@ -15,11 +20,24 @@ def test_diary_entry_count_words_example_2():
     expected = 4
     assert actual == expected
 
-one_hundred_words =  "one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one"
-
 def test_diary_entry_reading_time():
     diary_entry = DiaryEntry("One hundred words", one_hundred_words)
     # wpm = 100 words per minute
     actual = diary_entry.reading_time(100)
     expected = 1 # this is one minute
+    assert actual == expected
+
+def test_diary_entry_reading_chunk_for_100_words():
+    diary_entry = DiaryEntry("One hundred words", two_hundred_words)
+    actual = diary_entry.reading_chunk(100, 1)
+    expected = one_hundred_words
+    assert actual == expected
+
+def test_diary_entry_reading_chunk_for_100_words_twice():
+    diary_entry = DiaryEntry("One hundred words", mixture_of_words)
+    actual = diary_entry.reading_chunk(100, 1)
+    expected = one_hundred_words
+    assert actual == expected
+    actual = diary_entry.reading_chunk(100, 1)
+    expected = one_hundred_words_2
     assert actual == expected
